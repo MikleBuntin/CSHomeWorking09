3cd8eb828e73cf75f62a197dfcd927e0504692ea
MikleBuntin <BuntinMA@mail.ru>
2022-09-05 16:40:06 +0300

    Решил задачу 68. Ошибка в задании. Функция Аккермана от (2, 3) = 9 !!

---
+++ Task68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+// A(m, n) = A(m-1, A(m, n-1));
+
+int m = 2;
+int n = 3;
+
+int Akkerman(int m, int n)
+{
+    if(m == 0) return n + 1;
+    else if(n == 0) return Akkerman(m-1, 1);
+    else
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
+
+Console.WriteLine(Akkerman(m, n));
